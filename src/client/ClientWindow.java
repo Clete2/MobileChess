978af6e136cba7e064caef5142aca27fab17ec7c
Clete Blackwell II <clete2@gmail.com>
2010-11-07 01:17:03 -0400

    Can move pieces. Messy code.

--- src/client/ClientWindow.java
+++ src/client/ClientWindow.java
@@ -13,13 +13,14 @@ import shared.Game;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Vector;
 
 public class ClientWindow {
 
 	private JFrame frame;
-	private JPanel[] chessSquares;
-	private JLabel[] chessLabels;
-	private Game game;
+	private static JPanel[] chessSquares;
+	private static Vector<JLabel> chessLabels;
+	private static Game game;
 
 	/**
 	 * Launch the application.
@@ -72,26 +73,39 @@ public class ClientWindow {
 		mnFile.add(newGameMenuItem);
 
 		chessSquares = new JPanel[64];
-		chessLabels = new JLabel[64];
+		chessLabels = new Vector<JLabel>();
 
 		for(int i = 0; i < 64; i++) { // Sudo make me a grid
 			chessSquares[i] = new JPanel();
 			chessSquares[i].setBounds((i % 8 * 60) + 10, ((i / 8) * 60) + 35, 60, 60);
-			chessLabels[i] = new JLabel();
+			chessLabels.add(new JLabel());
 			if((i % 2 == 0 && (i / 8) % 2 == 0) || (i % 2 == 1 && (i / 8) % 2 == 1)) {
 				chessSquares[i].setBackground(Color.GRAY); // Does this make me look fat?
 			} else {
 				chessSquares[i].setBackground(Color.LIGHT_GRAY); // How about this?
 			}
-			chessSquares[i].add(chessLabels[i]);
+			chessLabels.get(i).addMouseListener(new MouseAdapter() {
+				@Override
+				public void mousePressed(MouseEvent e) {
+					game.getBoard().getPieceList().selectPieceOnLabel(
+							game.getBoard().getPieceList().getPieceAt(
+									(short)(chessLabels.indexOf(e.getSource()) / 8),
+									(short)(chessLabels.indexOf(e.getSource()) % 8)));
+				}
+			});
+			chessSquares[i].add(chessLabels.get(i));
 			frame.getContentPane().add(chessSquares[i]);
 		}
 	}
 
 	private void initializePieces() {
 		for(short i = 0; i < 64; i++) {
-			chessLabels[i].setIcon(game.getBoard().getPieceList().getImageForPiece((
+			chessLabels.elementAt(i).setIcon(game.getBoard().getPieceList().getImageForPiece((
 					game.getBoard().getPieceList().getPieceAt((short)(i / 8), (short)(i % 8)))));
 		}
+	}
+	
+	public static Vector<JLabel> getChessLabels() {
+		return chessLabels;
 	}
 }
--- src/shared/PieceList.java
+++ src/shared/PieceList.java
@@ -3,14 +3,17 @@ package shared;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
+import client.ClientWindow;
 
 public class PieceList {
 	private static final Exception PieceNotOnBoardException = null;
 	private static final Exception InvalidMoveException = null;
 	private Piece[][] pieces;
 	private MoveParser moveParse;
+	private Piece pieceSelected;
 
 	public PieceList() {
+		pieceSelected = null;
 		pieces = new Piece[8][8];
 
 		for(Short i = 0; i < 8; i++) {
@@ -36,13 +39,28 @@ public class PieceList {
 	}
 
 	public void movePiece(short newX, short newY, Piece pieceToMove, Player playerMoving) throws Exception {
+		// TODO Make sure that it is that player's turn.
+
+		short[] oldLocation = getPieceLocation(pieceToMove);
+
+		if(!moveParse.isMoveValid(newX, newY, pieceToMove, playerMoving)) {
+			throw InvalidMoveException;
+		}
+
+		updatePiece(oldLocation[0], oldLocation[1], newX, newY, pieceToMove);
+		pieces[oldLocation[0]][oldLocation[1]] = new Piece(PieceColor.NONE, PieceType.EMPTY);
+		pieces[newX][newY] = pieceToMove;
+	}
+
+	private short[] getPieceLocation(Piece pieceToSearchFor) throws Exception {
 		boolean found = false;
 		short x = -1;
 		short y = -1;
 
+		
 		for(Short i = 0; i < 8; i++) {
 			for(Short j = 0; j < 8; j++) {
-				if(pieces[i][j].equals(pieceToMove)) {
+				if(pieces[i][j].equals(pieceToSearchFor)) {
 					found = true;
 					x = i;
 					y = j;
@@ -57,23 +75,34 @@ public class PieceList {
 		if(!found) {
 			throw PieceNotOnBoardException;
 		}
-		if(!moveParse.isMoveValid(newX, newY, pieceToMove, playerMoving)) {
-			throw InvalidMoveException;
-		}
-
-		pieces[x][y] = new Piece(PieceColor.NONE, PieceType.EMPTY);
-		pieces[newX][newY] = pieceToMove;
+		
+		return new short[] {x, y};
 	}
 
-	public ImageIcon getImageForPiece(Piece piece) {
-		if(piece.getPieceType().toString().equalsIgnoreCase("EMPTY") ||
-				piece.getPieceColor().toString().equalsIgnoreCase("NONE")) {
-			return null;
-		}
-		
+	public void updatePiece(short oldX, short oldY, short newX, short newY, Piece pieceToUpdate) {
+		ClientWindow.getChessLabels().elementAt((oldX * 8) + oldY).setIcon(null);
+		ClientWindow.getChessLabels().elementAt((newX * 8) + newY).setIcon(getImageForPiece(pieceToUpdate));
+	}
+	
+	public ImageIcon getImageForPiece(Piece piece) {	
 		URL pieceURL = getClass().getResource("/images/" + 
 				piece.getPieceColor().toString() + 
 				piece.getPieceType().toString() + ".png");
 		return new ImageIcon(pieceURL);
+	}
+
+	public void selectPieceOnLabel(Piece pieceToWatch) {
+		if(pieceSelected == null) {
+			pieceSelected = pieceToWatch;
+			return;
+		}
+		short[] newLocation;
+		try {
+			newLocation = getPieceLocation(pieceToWatch);
+			movePiece(newLocation[0], newLocation[1], pieceSelected, new Player()); // TODO Change from new Player
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		pieceSelected = null;
 	}
 }
